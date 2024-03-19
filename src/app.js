e45fd8b1e06eb7c4fbef100fb88c7368b60730ca
Christian_Caballero <94983984+xtiancr78@users.noreply.github.com>
2024-03-19 17:20:14 -0500

    Update app.js
    se muestra la excusa en el segundo parrafo

--- src/app.js
+++ src/app.js
@@ -15,26 +15,29 @@ window.onload = function() {
 
 
   var enWho = Math.floor(Math.random() * who.length);
-var rValor1 = who[enWho];
-console.log(rValor1)
+  var rValor1 = who[enWho];
+  //console.log(rValor1)
 
 
-var enAction = Math.floor(Math.random() * action.length);
-var rValor2 = action[enAction];
-console.log(rValor2)
-
-
-var enWhat = Math.floor(Math.random() * what.length);
-var rValor3 = what[enWhat];
-console.log(rValor3)
-
-
-var enWhen = Math.floor(Math.random() * when.length);
-var rValor4 = when[enWhen];
-console.log(rValor4)
-
-console.log(rValor1+ rValor2+ rValor3+ rValor4)
+  var enAction = Math.floor(Math.random() * action.length);
+  var rValor2 = action[enAction];
+  //console.log(rValor2)
   
+  
+  var enWhat = Math.floor(Math.random() * what.length);
+  var rValor3 = what[enWhat];
+  //console.log(rValor3)
+  
+  
+  var enWhen = Math.floor(Math.random() * when.length);
+  var rValor4 = when[enWhen];
+  //console.log(rValor4)
+  
+  //console.log(rValor1 + " " + rValor2 + " " + rValor3 + " " + rValor4)
+  var cambiaExcusa = rValor1 + " " + rValor2 + " " + rValor3 + " " + rValor4;
+
+  document.getElementById("excuse").innerHTML = cambiaExcusa;
 };
+    
 
 
